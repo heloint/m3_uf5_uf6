9563a5513b7a1f4cf022bc207a448545e7353aa4
heloint <majerdaniel93@gmail.com>
2022-05-23 22:06:54 +0200

    test multi delim.

--- app.py
+++ app.py
@@ -21,12 +21,15 @@ def index():
     if request.method == 'POST':
         search_term:  str = request.form['searchTerm']
     
+    received_input: list[str] = re.split(r'[/\,;!]', search_term)
+
     headers: list[str] = ["".join(list(title)) for title in anime.get_query('title', search_term)]
     content: list[tuple] = anime.get_query('desc', search_term) 
     modeled_output: str = anime.model_result(content)
 
     data: list[dict] = [{header:modeled_output[i] for i, header in enumerate(headers)}]
     return render_template('index.html', data=data)
+#     return " ".join(received_input)
 
 # Main 
 # --------------------------------------------
