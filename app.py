d40af05b51d42f9f4db3815de8e5ffd63d759446
heloint <majerdaniel93@gmail.com>
2022-05-25 11:26:04 +0200

    test_v5 has succeded

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import utils
 import sqlite3
 import re
 import create_db as db
@@ -11,48 +12,6 @@ app: Flask = Flask(module_name)
 
 anime: db.DB = db.DB('./db_src/source.db')
 
-
-def get_headers(keywords: str) -> list[str]:
-    ''' Splits the search term into keywords and executes queries with each of them,
-        then removes repetitions from the results, returning a list of headers.
-    '''
-    received_keywords: list[str] = re.split(r'[/\,;!]', keywords)
-   
-    headers: list[str] = []
-    for keyword in received_keywords: 
-        titles: list[str] = ["".join(list(title)) for title in anime.get_query('title', keyword)]
-        headers += titles 
-
-    return headers
-
-
-def get_content(keywords: str) -> list[str]:
-    ''' Splits the search term into keywords and executes queries with each of them,
-        then removes repetitions from the results, returning a list of contents.
-    '''
-    
-    received_keywords: list[str] = re.split(r'[/\,;!]', keywords)
-   
-    contents: list[str] = []
-    for keyword in received_keywords: 
-        data: list[str] = ["".join(list(title)) for title in anime.get_query('desc', keyword)]
-        contents += data
-
-    return contents
-
-def remove_duplicates(source_data: list[dict]) -> list[dict]:
-    
-    collector: list[str] = []
-    data: dict = {}
-    
-    for key, value in source_data[0].items():
-        if value not in collector:
-            collector.append(value)
-            data[key] = value
-
-    return [data]
-
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     
@@ -62,8 +21,8 @@ def index():
     if request.method == 'POST':
         search_term:  str = request.form['searchTerm']
     
-    headers: list[str] = get_headers(search_term) 
-    content: list[str] = get_content(search_term) 
+    headers: list[str] = utils.get_headers(anime, search_term) 
+    content: list[str] = utils.get_content(anime, search_term) 
 
     if not headers or not content:
         return render_template('index.html', data = [{"Ooops ... Sorry !!!":"We couldn't find anything similar to what you wanted. Please try to re-phrase your search."}])
@@ -71,7 +30,7 @@ def index():
     modeled_output: list[str] = anime.model_result(content)
 
     data: list[dict] = [{header:modeled_output[i] for i, header in enumerate(headers)}]
-    data = remove_duplicates(data)
+    data = utils.remove_duplicates(data)
 
 
     return render_template('index.html', data = data)
